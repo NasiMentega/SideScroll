6ff51f7ef9a207ab1867d0403dbe5cfd1a0e6576
NasiMentega <rafaelww05@gmail.com>
2025-02-03 12:10:19 +0700

    Add Movement Feature
    Left and Right Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private float direction;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdatePosition();
+        SetDirection(Input.GetAxisRaw("Horizontal"));
+    }
+
+    private void UpdatePosition()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = direction * speed;
+        rb.velocity = velocity;
+    }
+
+    private void SetDirection(float direction)
+    {
+        this.direction = direction;
     }
 }
